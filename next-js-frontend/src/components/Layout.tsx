db835832e6665306bf4eb3cc6df0486f7c8a8a30
Mnlght0312 <Enkhmanlai.serious@gmail.com>
2023-04-10 16:44:48 +0800

    first push

--- next-js-frontend/src/components/Layout.tsx
+++ next-js-frontend/src/components/Layout.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { Meta } from "./Meta";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { Navbar } from "./Navbar";
+import { Roboto } from "next/font/google";
 
 export const Layout = ({ children }: any) => {
   return (
     <>
       <Meta />
+      <Navbar/>
       <Header />
       <main>{children}</main>
       <Footer />
     </>
   );
 };
+const font = Roboto({
+  subsets: ["latin"],
+  weight: "300"
+})
