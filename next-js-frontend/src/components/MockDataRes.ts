31671ef92e656d4a720def722333f6f3fbd368f9
Erdenedari <107394167+Daria61@users.noreply.github.com>
2023-04-12 19:10:12 +0800

    restaurant detail page draft

---
+++ next-js-frontend/src/components/MockDataRes.ts
@@ -0,0 +1,11 @@
+const Data = {
+  foodName: "Arig Ramen",
+  img: [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrAi8IKP-0B9Pjb_jTYeFfNSz7_QoUmHTJvMQdoA90uX0M3y5ICO-eC6pznXPjjCTX7i4&usqp=CAU",
+  ],
+  price: 14000,
+  foodType: "soup",
+  ingredients: ["max", "nogoo"],
+};
+
+export default Data;
---
+++ next-js-frontend/src/components/PopularDish.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import IRestaurant from "../pages/restaurant/index";
+import Data from "./MockDataRes";
+import Image from "next/image";
+
+export const PopularDish = ({ result }: { result: IRestaurant }) => {
+  const populer = [Data, Data, Data, Data, Data, Data];
+  return (
+    <div className="">
+      <div className="grid grid-cols-2 sm:grid-cols-3 p-5 text-white">
+        {populer.map((item, ind) => {
+          return (
+            <div
+              key={ind}
+              className={
+                ind == 1 || ind == 4 ? "m-2 mt-16 hidden sm:block " : "m-2"
+              }>
+              <div>
+                <img src={item.img[0]} alt="img" className="w-full" />
+              </div>
+              <div className="text-center m-5">
+                <h1 className="text-xl font-light uppercase">
+                  {item.foodName}
+                </h1>
+                <p className="text-xl font-light">Restaurant name</p>
+                <div className="flex justify-between font-thin">
+                  <p>rate by start here</p>
+                  <p>rate by number</p>
+                  <p>/</p>
+                  <p>review</p>
+                </div>
+                <p>Price: 34525</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
