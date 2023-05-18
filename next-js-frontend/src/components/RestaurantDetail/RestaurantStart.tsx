700777caa9435720ac4f62b051054d1726c36df7
Mnlght0312 <Enkhmanlai.serious@gmail.com>
2023-05-18 19:21:05 +0800

    RestaurantStart fix

--- next-js-frontend/src/components/RestaurantDetail/RestaurantStart.tsx
+++ next-js-frontend/src/components/RestaurantDetail/RestaurantStart.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { IDetailRest } from "../InterfaceEnumsMeta/InterFace";
 import Stars from "../commentsRates/Stars";
 import { AiFillClockCircle } from "react-icons/ai";
+import gip from "../../img/gip.gif";
 
 export const RestaurantStart = ({
   restaurant,
@@ -25,32 +26,41 @@ export const RestaurantStart = ({
           height={600}
         />
       </div>
-      <div className="md:basis-1/3 flex justify-around items-center">
-        <div className="font-3xl md:font-2xl lg:w-96 ">
+      <div className="md:basis-2/3 flex justify-around items-center">
+        <div className="font-3xl md:font-2xl relative">
+          <Image
+            width={400}
+            height={400}
+            alt="gphy"
+            src={gip}
+            className="pic"
+          />
           <div className="con">
             <div className="cloud"></div>
           </div>
-          <h1 className="m-5 text-3xl uppercase">{restaurantName} </h1>
-          <p className="hidden md:block text-start font-light text-sm">
-            {description}
-          </p>
-          <div className="flex gap-3 my-3 items-center">
-            <Stars stars={restaurant?.avg} />
-            <p>/</p>
-            <p>Reviewer : {restaurant?.count}</p>
-          </div>
-          <div className="flex gap-2 items-center">
-            <p className="text-3xl">
-              <AiFillClockCircle />
+          <div className="picsl absolute top-[20%] left-[25%]">
+            <h1 className="m-5 text-3xl uppercase">{restaurantName} </h1>
+            <p className="hidden md:block text-start font-light text-sm">
+              {description}
             </p>
-            <div>
-              <div className="flex text-sm font-light">
-                Mon-Fri: {schedule?.weekday?.open}~{schedule?.weekday?.close}{" "}
-                clock
-              </div>
-              <div className="flex text-sm font-light">
-                Weekend: {schedule?.weekday?.open}~{schedule?.weekday?.close}{" "}
-                clock
+            <div className="flex gap-3 my-3 items-center justify-center">
+              <Stars stars={restaurant?.avg} />
+              <p>/</p>
+              <p>Reviewer : {restaurant?.count}</p>
+            </div>
+            <div className="flex gap-2 items-center justify-center">
+              <p className="text-3xl">
+                <AiFillClockCircle />
+              </p>
+              <div>
+                <div className="flex text-sm font-light">
+                  Mon-Fri: {schedule?.weekday?.open}~{schedule?.weekday?.close}{" "}
+                  clock
+                </div>
+                <div className="flex text-sm font-light">
+                  Weekend: {schedule?.weekday?.open}~{schedule?.weekday?.close}{" "}
+                  clock
+                </div>
               </div>
             </div>
           </div>
